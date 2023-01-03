f2989d5033a6852bf9b4afcf2d364c9c83c254ef
Iryna Kuziv <nute7021@gmail.com>
2023-01-03 21:30:15 +0200

    add js, fixed mixstaces

--- js/script.js
+++ js/script.js
@@ -15,3 +15,104 @@ $(document).ready(function(){
         ]
     });
 });
+
+$('ul.catalog_tabs').on('click', 'li:not(.catalog_tab_active)', function() {
+    $(this)
+      .addClass('catalog_tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog_content').removeClass('catalog_content_active')
+      .eq($(this).index()).addClass('catalog_content_active');
+});
+
+function toggleSlide(item) {
+    $(item).each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item_content').eq(i).toggleClass('catalog-item_content_active');
+            $('.catalog-item_list').eq(i).toggleClass('catalog-item_list_active');
+        });
+    });
+}
+
+toggleSlide('.catalog-item_link');
+toggleSlide('.catalog-item_back');
+
+// Modal
+
+$('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+});
+$('.modal_close').on('click', function() {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+});
+
+$('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+        $('#order .modal_descr').text($('.catalog-item_subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+    });
+});
+
+function validateForms(form){
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Будь ласка, введіть своє ім'я",
+                minlength: jQuery.validator.format("Введіть {0} символи!")
+              },
+            phone: "Будь ласка, введіть свій номер телефону",
+            email: {
+              required: "Будь ласка, введіть свою пошту",
+              email: "Невірно введена адреса пошти"
+            }
+        }
+    });
+}
+
+validateForms('#consultation-form');
+validateForms('#consultation form');
+validateForms('#order form');
+
+$('input[name=phone]').mask("+38 (999) 999-99-99");
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
+
+//Smooth scroll and pageup
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+$("a[href^='#']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
